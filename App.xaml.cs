03baec0429cb84895cfed8988f7ef196805cfe2d
German <e1717673170@uisrael.edu.ec>
2024-06-05 23:04:25 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GaguirreClaseS2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ View/vCalificaciones.xaml.cs
@@ -0,0 +1,9 @@
+namespace GaguirreClaseS2.View;
+
+public partial class vCalificaciones : ContentPage
+{
+	public vCalificaciones()
+	{
+		InitializeComponent();
+	}
+}
